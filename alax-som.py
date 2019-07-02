275c68cd103b2b6060c475e0912ed8211a1493a8
alaxalves <alaxallves@gmail.com>
2019-07-02 14:27:07 -0300

    Adding SOM

---
+++ alax-som.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('us-weather-history-KPHX.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len = 11, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
+
+mappings = som.win_map(X)
+frauds = np.concatenate((mappings[(7,3)], mappings[(7,4)]), axis = 0)
+frauds = sc.inverse_transform(frauds)
+
+print(frauds)
