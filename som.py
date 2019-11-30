833fac22640dbf1b15fd68ef93219e4c7502113a
Gabriel Albino <enggabrielalbino@gmail.com>
2019-11-30 14:18:32 -0300

    finalizando exercicio

--- som.py
+++ som.py
@@ -1,6 +1,9 @@
 # Self Organizing Map
 
 # Importing the libraries
+from pylab import bone, pcolor, colorbar, plot, show
+from minisom import MiniSom
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -11,20 +14,17 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Feature Scaling
-from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
+sc = MinMaxScaler(feature_range=(0, 1))
 X = sc.fit_transform(X)
 
 # Training the SOM
-from minisom import MiniSom
-som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som = MiniSom(x=10, y=10, input_len=15, sigma=1.0, learning_rate=0.5)
 som.random_weights_init(X)
-som.train_random(data = X, num_iteration = 100)
+som.train_random(data=X, num_iteration=1000)
 
 # Visualizing the results
-from pylab import bone, pcolor, colorbar, plot, show
 bone()
-#pcolor(som.distance_map())
+# pcolor(som.distance_map())
 pcolor(som.distance_map().T)
 colorbar()
 markers = ['o', 's']
@@ -34,14 +34,14 @@ for i, x in enumerate(X):
     plot(w[0] + 0.5,
          w[1] + 0.5,
          markers[y[i]],
-         markeredgecolor = colors[y[i]],
-         markerfacecolor = 'None',
-         markersize = 10,
-         markeredgewidth = 2)
+         markeredgecolor=colors[y[i]],
+         markerfacecolor='None',
+         markersize=10,
+         markeredgewidth=2)
 show()
 
 # Finding the frauds
 mappings = som.win_map(X)
-frauds = np.concatenate((mappings[(7,3)], mappings[(7,4)]), axis = 0)
+frauds = np.concatenate((mappings[(8, 4)], mappings[(7, 4)]), axis=0)
 frauds = sc.inverse_transform(frauds)
 # change the float format to %.3f
